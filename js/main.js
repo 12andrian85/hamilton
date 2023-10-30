9a63c900f68eb3023b92e890e6c4d316e86c9961
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2023-10-30 22:03:38 +0200

    first

--- js/main.js
+++ js/main.js
@@ -4,4 +4,60 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Custom JS here
-console.log('hello')
+// console.log('hello')
+
+var splide = new Splide('.splide',  {
+    direction: 'ttb',
+    heightRatio : 0.3,
+    width: '95%',
+    type: 'loop',
+    wheel: "true",
+});
+splide.mount();
+
+
+var splide = new Splide('#popular',  {
+    type: 'loop',
+    perPage: 3,
+    focus: 'center',
+    arrows: 'false',
+    breakpoints: {
+        640: {
+          perPage: 1,
+        },
+        768: {
+          perPage: 2,
+        },
+        // 1200: {
+        //   perPage: 3,
+        // },
+        2560: {
+          perPage: 3,
+        },
+      }
+
+});
+splide.mount();
+
+var splide = new Splide('#partners',  {
+    type: 'loop',
+    perPage: 3,
+    focus: 'center',
+    arrows: 'false',
+    breakpoints: {
+        640: {
+          perPage: 1,
+        },
+        768: {
+          perPage: 3,
+        },
+        1200: {
+          perPage: 5,
+        },
+        2560: {
+          perPage: 5,
+        },
+      }
+
+});
+splide.mount();
